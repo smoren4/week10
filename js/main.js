84016a638c783d0c4856a86b52da0740efba7649
Sal Moreno <smoreno5@neiu.edu>
2024-03-20 12:48:09 -0500

    updated

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { getFirstObjectWithName } from './RayCastHelper.js';
 
 var width = window.innerWidth;
 var height = window.innerHeight;
@@ -20,7 +21,7 @@ camera.rotation.x = -0.1;
 
 // 3: create a renderer
 var renderer = new THREE.WebGLRenderer({antialias: true});
-renderer.setClearColor("#e5e5e5");
+renderer.setClearColor("#141414");
 renderer.setSize(width, height);
 document.body.appendChild(renderer.domElement);
 
@@ -30,12 +31,15 @@ var geometry = new THREE.BoxGeometry(1, 1, 1);
 var material = new THREE.MeshStandardMaterial({color: 0x0000ff, metalness: 0.9, roughness: 0});
 var cube = new THREE.Mesh(geometry,material);
 scene.add(cube);
+cube.name = "cube";
 
 const cubeX = 0;
 const cubeY = 2;
 const cubeZ = -2;
 cube.position.set(cubeX,cubeY,cubeZ);
 cube.scale.x = 2;
+
+
 
 //add plane
 const geometryPlane = new THREE.PlaneGeometry( 1, 1 );
@@ -44,8 +48,51 @@ const plane = new THREE.Mesh( geometryPlane, materialPlane);
 scene.add( plane );
 
 plane.rotation.x = THREE.MathUtils.degToRad(90);
-plane.scale.x = 60;     
-plane.scale.y = 60;
+plane.scale.x = 40;     
+plane.scale.y = 40;
+
+
+
+
+//*************LIGHTS *************//
+
+var lightSize = 10;
+// 5: Add lighting to the scene
+var pointLight = new THREE.PointLight(0xFFFFF,lightSize,200)
+pointLight.position.set(cubeX,cubeY+2,cubeZ);
+scene.add(pointLight);
+
+//Add light helper
+const pointLightHelper = new THREE.PointLightHelper(pointLight, lightSize);
+// scene.add(pointLightHelper);
+
+
+//add ambient light
+var ambientLight = new THREE.AmbientLight(0x404040, 2);
+// scene.add(ambientLight);
+
+//add directional light
+var directionalLight = new THREE.DirectionalLight(0xffffff, 1);
+// scene.add(directionalLight);
+directionalLight.target.position.set(1,0,-1);
+
+//add directional light helper
+const directionalLightHelper = new THREE.DirectionalLightHelper(directionalLight, 1);
+scene.add(directionalLightHelper);
+
+// Adding Orbit Controls
+const controls = new OrbitControls(camera, renderer.domElement);
+// controls.zoomToCursor = true;
+controls.autoRotate = true;
+
+
+//resize window
+window.addEventListener('resize', () => {
+        renderer.setSize(window.innerWidth,window.innerHeight);
+        camera.aspect = window.innerWidth / window.innerHeight;
+       
+        camera.updateProjectionMatrix();
+});
 
 
 //declare variables for model
@@ -72,64 +119,61 @@ gltfLoader.load('media/models/phoenix_bird.glb', (gltf) => {
 });
 
 
-
-
-
-
-var lightSize = 10;
-// 5: Add lighting to the scene
-var pointLight = new THREE.PointLight(0xFFFFF,lightSize,200)
-pointLight.position.set(cubeX,cubeY+2,cubeZ);
-// scene.add(pointLight);
-
-//Add light helper
-const pointLightHelper = new THREE.PointLightHelper(pointLight, lightSize);
-// scene.add(pointLightHelper);
-
-
-//add ambient light
-var ambientLight = new THREE.AmbientLight(0x404040, 2);
-// scene.add(ambientLight);
-
-//add directional light
-var directionalLight = new THREE.DirectionalLight(0xffffff, 1);
-scene.add(directionalLight);
-directionalLight.target.position.set(0,0,0);
-
-//add directional light helper
-const directionalLightHelper = new THREE.DirectionalLightHelper(directionalLight, 1);
-scene.add(directionalLightHelper);
-
-// Adding Orbit Controls
-const controls = new OrbitControls(camera, renderer.domElement);
-// controls.zoomToCursor = true;
-controls.autoRotate = true;
-
-
-//resize window
-window.addEventListener('resize', () => {
-        renderer.setSize(window.innerWidth,window.innerHeight);
-        camera.aspect = window.innerWidth / window.innerHeight;
-       
-        camera.updateProjectionMatrix();
-});
-
-
-const clock = new THREE.Clock();
-
 // FINAL: Render the scene
 function animate(){
     requestAnimationFrame(animate);
 
     cube.rotation.x += 0.01;
     cube.rotation.y += 0.01;
-
-    bird.position.x += 0.02;
-
-    var delta = clock.getDelta();
-    mixer.update( delta );
+    
+    bird.position.x += 0.04;
 
     renderer.render(scene,camera);
 }
 
 animate();
+
+
+
+
+
+
+
+
+
+
+
+//add plane
+// const geometryPlane2 = new THREE.PlaneGeometry( 1, 1 );
+// const materialPlane2 = new THREE.MeshLambertMaterial( {color: "#61baff", side: THREE.DoubleSide} );
+// const plane2 = new THREE.Mesh( geometryPlane2, materialPlane2);
+// scene.add( plane2 );
+
+// // plane2.rotation.x = THREE.MathUtils.degToRad(90);
+// plane2.scale.x = 40;     
+// plane2.scale.y = 40;
+// plane2.position.z = -20;
+
+// //add plane
+// const geometryPlane3 = new THREE.PlaneGeometry( 1, 1 );
+// const materialPlane3 = new THREE.MeshLambertMaterial( {color: "#61baff", side: THREE.DoubleSide} );
+// const plane3 = new THREE.Mesh( geometryPlane3, materialPlane3);
+// scene.add( plane3 );
+
+// plane3.rotation.y = THREE.MathUtils.degToRad(90);
+// plane3.scale.x = 40;     
+// plane3.scale.y = 40;
+// plane3.position.x = -20;
+
+
+
+// //add event listener to cube
+// document.addEventListener('click', onClick);
+
+// function onClick(){
+//     const obj = getFirstObjectWithName(event, window, camera, scene, "cube");
+//     if(obj != null){
+//         console.log("Cube clicked");
+//         cube.material.color.set("#30ff49");
+//     }
+// }
